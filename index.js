d29ab497621d6e173ba1734945f2ae7fe1f3b290
glockstory <danila1025@gmail.com>
2021-07-14 13:12:07 +0300

    fixed queries

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express ()
 const bodyParser = require('body-parser')
 require('./mongodb')
 const activityModel = require('./models')
+const { ObjectID } = require('mongodb')
 app.use(bodyParser.json())
 
 // get week:
@@ -17,18 +18,18 @@ app.get('/week', (req,res) => {
 // поиск всех активностей
 app.get('/activities', async (req, res)=> {
     const activities = await activityModel.find({})
-    res.send(activities)
+    res.status(200).send(activities)
 })
 
 app.get('/activity/:id', async (req, res)=>{
     const id = req.params.id
 
     const activity = await activityModel.findOne({
-        _id: id
+        _id: new ObjectID(id)
     })
 
     if (activity){
-        res.send(activity)
+        res.status(200).send(activity)
     } else {
         res.status(404).send('Activity not found')
     }
@@ -42,9 +43,9 @@ const authMiddleware = (req,res,done) => {
 }
 
 // put - обновление активностей
-app.put('/activity',(req,res) => {
+app.put('/activities/:id',(req,res) => {
     const {name, time, pictogram, repeat, remind} = req.body
-
+    console.log(name, time, pictogram)
     if(!name || !time || !pictogram){
         res.status(500).send('Name or time or pictogram is empty')
     } else {
